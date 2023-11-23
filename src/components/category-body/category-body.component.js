f95de0046d15757e68c299b5095d55721dac38fa
Nathan <nathanbark4@gmail.com>
2023-11-23 21:09:53 +0200

    Update category body and list  components

---
+++ src/components/category-body/category-body.component.js
@@ -0,0 +1,19 @@
+import './categoru-body.styles.scss'
+
+const CategoryBody = ({category}) => {
+    const { title, imageUrl} = category;
+
+    return (
+        <div className="category-container">
+          <div className="background-image" style={{
+            backgroundImage: `url(${imageUrl})`
+          }}/>
+          <div className="category-body-container">
+            <h2>{title}</h2>
+            <p>Shop Now</p>
+          </div>
+        </div>
+    )
+}
+
+export default CategoryBody
